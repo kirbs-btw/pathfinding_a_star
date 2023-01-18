66f254998a6b0b257906cfbe0105bea884609897
Bastian Lipka <68615019+kirbs-btw@users.noreply.github.com>
2023-01-18 17:16:11 +0100

    Add files via upload

--- vis.py
+++ vis.py
@@ -1,6 +1,7 @@
 import tkinter as tk
+from pathfinding import *
 
-def visualisation():
+def visualisation(field):
     root = tk.Tk()
 
     len = 500
@@ -12,12 +13,38 @@ def visualisation():
 
     root.mainloop()
 
-def placeBlocks(frame, field):
-    
-    for row in field:
+def placeBlocks(frame, field):  
+    color = ""
+    for row in field.grid:
         for object in row:
-            
-    
+            if type(object) != Node:
+                if type(object) == End_node:
+                    color = "#0ff513"
+                elif type(object) == Start_node:
+                    color = "#f50f2e"
+                elif type(object) == Path_node:
+                    color = "#4d5beb"
+                elif type(object) == Obstacle:
+                    color = "#000000"
+
+                block = tk.Frame(frame, bg=color, width=50, height=50)
+                block.place(x = (object.pos_x * 50), y = (object.pos_y * 50))
+    frame.update()
+
 
 if __name__ == '__main__':
-    visualisation()
+    field = Grid(5)         # init grid with empty nodes
+    
+    start_node = Start_node(0, 0)
+    end_node = End_node(4, 4)
+
+    test = Node(0, 1)
+
+    wall1 = Obstacle(2, 2)
+    wall2 = Obstacle(2, 3)
+    wall3 = Obstacle(2, 1)
+    wall4 = Obstacle(2, 0)
+
+    field.positionNode(start_node, end_node, wall1, wall2, wall3, wall4)
+
+    visualisation(field)
